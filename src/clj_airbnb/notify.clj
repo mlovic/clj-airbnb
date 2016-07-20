81b08b00a2eecde04bf50d223d2a1e1979e077e8
Marko Lovic <markolovic@hotmail.com>
2016-07-20 17:33:57 +0200

    Wrap notify! in try block to log email errors

--- src/clj_airbnb/notify.clj
+++ src/clj_airbnb/notify.clj
@@ -5,12 +5,15 @@
 (defn notify! 
   "Notify about change"
   [changes]
-  (let [id (:id (first changes))
-        msg (str "There have been changes: " changes)]
-    (postal/send-message {:host (env :smtp-host)}
-                         {:from (env :mail-from) 
-                          :to   (env :mail-to) 
-                          :subject (str "Change(s) for " id)
-                          :body msg
-                          :X-Tra "Something else"}))
+  (try
+    (let [id (:id (first changes))
+          msg (str "There have been changes: " (doall changes))]
+      (postal/send-message {:host (env :smtp-host)}
+                           {:from (env :mail-from) 
+                            :to   (env :mail-to) 
+                            :subject (str "Change(s) for " id)
+                            :body msg
+                            :X-Tra "Something else"}))
+    (catch Exception e
+      (prn "Error sending email!  " e)))
   )
