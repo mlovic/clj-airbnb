4b51de23e40f973d369aaf6c476d6b3123ff4e6c
Marko Lovic <markolovic@hotmail.com>
2016-07-29 19:35:40 +0200

    Add functionality to add search-alert to system

--- src/clj_airbnb/core.clj
+++ src/clj_airbnb/core.clj
@@ -25,6 +25,7 @@
         ;new-days (difference (map :date old) (map :date nu));(find-new-days old nu)
         ;; TODO push more of functionality below to change ns
         changes (->> (cal/find-changes old nu)
+                     ;; implement as comp'd transducers?
                      (map #(assoc % :id id))
                      (map #(assoc % :change_seen (java.util.Date.))))]
     (log/info "Found " (count changes) " changes for " id )
@@ -32,9 +33,9 @@
       (do 
         (log/info "Sending email for changes in " id)
         (notify/notify! (into () (doall changes))) ; convert lazyseq to list 
+        (log/debug "persisting changes...")
         (go (doseq [change changes] 
               #_(>! c change) ;TODO get rid of channels
-              (log/debug "persisting changes...")
               (store/persist (change/map->Change change))))
         (store/update-calendar id nu)
         )
@@ -91,3 +92,105 @@
         (add-listing (:id alert)))
       (store/persist (alert/map->Alert alert)) ; TODO consider if alert already exists
       (sched/add-alert-to-queue alert-queue alert))))
+
+(require 'clj-time.format)
+(require '[clj-time.core :as t])
+;; maybe do with infinite lazy seq of dates. iterator
+(defn dates-between 
+  "Returns a list of all dates between start (inclusive) and end (exclusive)." ;vec?
+  [start end] 
+  (loop [dates [] 
+         current start]
+    (if (= current end) 
+      dates
+      (recur (conj dates current) 
+             (t/plus- current (t/days 1))))))
+
+(defn get-dates
+  "Builds the list of dates from check in/out params in airbnb search-query" 
+  [{checkin-str "checkin" checkout-str "checkout"}] ; can omit query?
+  (let [formatter (clj-time.format/formatter "MM/dd/YY")
+        parse    (fn [string] (clj-time.format/parse-local-date formatter string))
+        checkin  (parse checkin-str)
+        checkout (parse checkout-str)]
+    (dates-between checkin checkout)
+    ;(-> '(checkin checkout)
+         ;(map parse)
+         ;(apply dates-between))
+    ))
+
+(defn- enq-listings 
+  [listings channel]
+  (doseq [listing listings]
+    (>!! channel listing)))
+
+(defn get-listings
+  "Takes query and returns a channel, where it will putting each listing.
+  Closes when finished"
+  ([query num-listings]
+   (let [out (chan 5)]
+     (go 
+       (get-listings query num-listings 0 out))
+     out))
+  ([query num-listings offset out]
+   (let [batch-size 50 ; move constant to airbnb ns?
+         enq (fn [listings] (log/debug "Putting listings " (map :id listings) " on channel" ) (doseq [listing listings] (>!! out listing)))]
+     (if (< num-listings batch-size) 
+       (do
+         (enq (airbnb/search-listings query num-listings offset))
+         (close! out))
+       (do
+         (enq (airbnb/search-listings query batch-size offset)) 
+         (get-listings query (- num-listings batch-size) (+ offset batch-size) out))))))
+
+(comment
+  (defn get-listings
+    "Takes query and returns a channel, where it will putting each listing.
+    Closes when finished"
+    [query max-listings]
+    (let [out (chan 5)
+          batch-size 50] ; move constant to airbnb ns?
+      (go
+        (loop [offset 0])
+        (doseq [offset (range 0 max-listings batch-size)]
+          (doseq [listing (airbnb/search-listings query batch-size offset)]
+            (log/debug "Putting listing " (:id listing) " on channel" )
+            (>! out listing))))
+      out)))
+
+;; QUERY-ALERT       [freq dates ids params/query]
+;; (Listing-?)Alert  [freq dates id]
+
+;; TODO
+;; take aq stuff out of sched
+;; rethink Persistable protocol and where to put record defs
+;; how going to store query?
+;; make the long work async, and return fast on first success
+(defn add-search-alert
+  "Add search query alert to system"
+  [location raw-query] ;; TODO figure this out
+  (let [freq 120 ; TODO 
+        dates (or nil (get-dates raw-query)) ;; TODO dates
+        query (assoc raw-query :location location)
+        max-listings 3] ; TODO 
+    (log/info "Adding search alert with freq dates query:" freq dates query)
+    (let [listings-chan (get-listings query max-listings)]
+      (->
+        (loop [ids []] 
+          (if-let [li (<!! listings-chan)] ;put this directly in (go)-loop? or recur
+            (do
+              (log/info "Taking listing " (:id li) " off channel" )
+              ;; have info, only need cal. But info from here and /listings is different
+              ;; If listing already exists, update it without notification
+              (add-listing (:id li))
+              ;; TODO try to remove dependency on sched. Maybe w/protocol.
+              (sched/add-alert-to-queue alert-queue {:freq freq 
+                                                     :id (:id li)
+                                                     :dates (set dates)})
+              (recur (conj ids (:id li))))
+            ids))
+        ;; fix this mess. Or make async and reorder
+        (#(store/persist (alert/->SearchAlert freq dates % query)))))))
+
+#_(defn notify? [{:keys [listing-id date]} change]
+    (some #{date} (:dates (listing-id alert-queue)))) ; change name to alerts?
